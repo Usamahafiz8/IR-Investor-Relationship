581f9d85056ddf6a8123ebc42212c8b3917055f3
Usamahafiz8 <usamahafiz8@gmail.com>
2024-04-03 10:59:14 +0500

    uploading template

---
+++ src/data/heroSlideHome-data.ts
@@ -0,0 +1,29 @@
+import thumb1 from "../../public/assets/img/bg/hero-bg-2.jpg"
+import thumb2 from "../../public/assets/img/bg/hero-bg-3.jpg"
+import thumb3 from "../../public/assets/img/bg/room-view.jpg"
+
+const heroSlideHome2 = [
+    {
+        id:1,
+        img:thumb1,
+        subTitle:"THE NEW GENERATION OF LUXURY",
+        title:"Welcome to Royel:",
+        titleBr:"A Haven of Luxury and Serenity"
+    },
+    {
+        id:2,
+        img:thumb2,
+        subTitle:"THE NEW GENERATION OF LUXURY",
+        title:"Luxury Redefined:",
+        titleBr:"Welcome to Royel"
+    },
+    {
+        id:3,
+        img:thumb3,
+        subTitle:"THE NEW GENERATION OF LUXURY",
+        title:"Experience the Ultimate in",
+        titleBr:"Luxury at Royel"
+    },
+]
+
+export default heroSlideHome2
